c03c37e6b8f6cbf556fab327176960eee4e4e92e
Ryan Sherman <shermr@rpi.edu>
2015-11-28 08:30:04 -0500

    fixed another minor bug

--- py_files/tf_test_advanced.py
+++ py_files/tf_test_advanced.py
@@ -44,7 +44,7 @@ h_pool1 = max_pool_2x2(h_conv1)
 W_conv2 = weight_variable([5, 5, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = mx_pool_2x2(h_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
 #the image has been reduced to 7x7
 #use a fully connected layer with 1024 neurons to process the entire image
 W_fc1 = weight_variable([7*7*64, 1024])
