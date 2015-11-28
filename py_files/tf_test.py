ba4cd42434419bd05f3c7e7e2521dae6ee36f463
Ryan Sherman <shermr@rpi.edu>
2015-11-28 04:14:40 -0500

    forgot a line, frequent commits due to testing over ssh

--- py_files/tf_test.py
+++ py_files/tf_test.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 x = tf.placeholder("float", [None, 784])
 W = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x,W) + b)
 y_ = tf.placeholder("float", [None,10])
 cross_entropy = -tf.reduce_sum(y_*tf.log(y))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
