023997cc267118aca341f2acb07e9bfe2197e4f9
Ryan Sherman <shermr@rpi.edu>
2015-12-12 13:53:26 -0500

    changed download

--- py_files/cifar10.py
+++ py_files/cifar10.py
@@ -78,7 +78,7 @@ INITIAL_LEARNING_RATE = 0.1       # Initial learning rate.
 # names of the summaries when visualizing a model.
 TOWER_NAME = 'tower'
 
-DATA_URL = 'http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz'
+DATA_URL = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
 
 
 def _activation_summary(x):
